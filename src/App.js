ef4c5eaf538d55e0863433dda2ee5f1aacab3459
monica88green <71725844+monica88green@users.noreply.github.com>
2021-01-07 15:42:06 -0700

    Added bootstrap dependency

--- src/App.js
+++ src/App.js
@@ -7,8 +7,25 @@ export default function App() {
     
         <h1>
           Hello from Monica's Resume
-
         </h1>
+        
+        <h3>
+          HTML5 / CSS / ES6 / GIT / REACT 
+        </h3>
+        <ul>
+          <li>
+            + Who am I?
+          </li>
+          <li>
+            + What is my professional experience?
+          </li>
+          <li>
+            + What are my goals?
+          </li>
+          <li>
+            What's next?
+          </li>
+        </ul>
 
     </div>
   );
