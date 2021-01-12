504e1f024062a690a568b6af2b12fdc64fa904d8
monica88green <71725844+monica88green@users.noreply.github.com>
2021-01-12 16:21:26 -0700

    Added content to About Me component

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function AboutMe() {
+let [ready, setReady] = useState(false);
+  function handleClick(event) {
+    event.preventDefault();
+    setReady(true);
+  }
+  if ({ready}) {
+    return (
+      <div className="AboutMe">
+        <p>
+          Originally from Southern California but I call beautiful Colorado home. I spend as much time as possible outdoors exploring trails and mountain sides. When I'm not outdoors I love cooking and working on various crafting projects. 
+        </p>
+      </div>
+    )
+     } else {
   return (
     <div>
-      + About Me
+     <a href="/" onclick={handleClick}> + About Me </a>
     </div>
   )
-}
+} 
+  }
